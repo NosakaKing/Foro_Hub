50facdbcdf1f7bc720b3b469019bfde0c1760468
Nosaka King <raulduran2808@gmail.com>
2025-08-14 14:30:52 -0500

    feat: add changes

---
+++ forohub/src/main/resources/db/migration/V10__insert_data_course.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO cursos (nombre, descripcion, estado) VALUES
+  ('Desarrollo Backend Java', 'Curso de programación backend con Java y Spring Boot', 'ACTIVO'),
+  ('Desarrollo Frontend React', 'Curso de desarrollo frontend con React y Next.js', 'ACTIVO'),
+  ('Bases de Datos SQL', 'Curso de diseño y gestión de bases de datos relacionales', 'ACTIVO');
