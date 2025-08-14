92b650d8d5ac20ec347345109f1d7e132fa70ac6
Nosaka King <raulduran2808@gmail.com>
2025-08-13 19:41:18 -0500

    feat: Secure False for Development

--- forohub/src/main/java/com/forohub/forohub/infra/util/CookieUtil.java
+++ forohub/src/main/java/com/forohub/forohub/infra/util/CookieUtil.java
@@ -6,18 +6,20 @@ public class CookieUtil {
     public static Cookie createAccessTokenCookie(String token, int maxAgeSeconds) {
         Cookie cookie = new Cookie("accessToken", token);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(maxAgeSeconds);
+        cookie.setAttribute("SameSite", "Lax");
         return cookie;
     }
 
     public static Cookie clearAccessTokenCookie() {
         Cookie cookie = new Cookie("accessToken", null);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(0);
+        cookie.setAttribute("SameSite", "Lax");
         return cookie;
     }
 
